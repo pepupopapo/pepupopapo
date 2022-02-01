550ac0f0d870116ed8b013f9b49100111ff2f118
pepupopapo <98745950+pepupopapo@users.noreply.github.com>
2022-02-01 11:09:25 +0900

    JS updated

--- application.js
+++ application.js
@@ -1,24 +1,22 @@
-if(typeof jQuery != "undefined"){ //jQueryの読み込み確認
-    $(function(){
-        alert('jQuery is ready. Version:' + $.fn.jquery);
-    });
-}
+$(document).ready( function(){
+    alert('jQuery is ready. Version:' + $.fn.jquery);
+});
 
 $(function(){
-    //.sampleをクリックしてajax通信を行う
     $('.sample_btn').click(function(){
         $.ajax({
             url: 'https://zipcloud.ibsnet.co.jp/api/search?zipcode=1506003',
             type: 'GET',
-            dataType: 'html'
-        }).done(function(data){
-            /* 通信成功時 */
-            $('.result').html(data); //取得したHTMLを.resultに反映
-            
-        }).fail(function(data){
-            /* 通信失敗時 */
-            alert('通信失敗！');
-                    
+            dataType: 'html',
+            success: function(data){
+                /* 通信成功時 */
+                $('.result').html(data); //取得したHTMLを.resultに反映
+            },
+            error: function( data ) {
+                window.alert("soko");
+                /* 通信失敗時 */
+                alert('通信失敗！');
+            }
         });
     });
 });
